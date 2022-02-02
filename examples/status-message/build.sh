67ab9d63f151c81c14e4e927d995209e41791891
Willem Wyndham <wyndham@cs.unc.edu>
2022-02-02 10:30:54 -0500

    chore: update wit-bindgen-cli

--- examples/status-message/build.sh
+++ examples/status-message/build.sh
@@ -3,7 +3,7 @@ TARGET="${CARGO_TARGET_DIR:-target}"
 set -e
 cd "`dirname $0`"
 cargo install cargo-witgen
-cargo install --git https://github.com/willemneal/witx-bindgen --rev afaa3451aebb665e40c28031fe6176374bce6760 wit-bindgen-cli
+cargo install --git https://github.com/ahalabs/wit-bindgen --rev e37ff328de87bcb222c923396c91def0db5e9b0f wit-bindgen-cli
 cargo build --target wasm32-unknown-unknown --release
 cp $TARGET/wasm32-unknown-unknown/release/status_message.wasm ./res/
 #wasm-opt -Oz --output ./res/status_message.wasm ./res/status_message.wasm
